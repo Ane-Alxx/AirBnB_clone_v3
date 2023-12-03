e41aaf3febcd385b481930d96e5a759b688b9b50
Ane-A <53179280+Ane-A@users.noreply.github.com>
2023-12-03 13:49:58 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,13 +7,10 @@ solution for task 3(index.py):
 """
 
 from api.v1.views import app_views
-
 from flask import Flask, jsonify
+from models import storage
 
-
-
-@app_views.route('/status', strict_slashes=False, methods=["GET"])
-
+@app_views.route("/status", strict_slashes=False, methods=["GET"])
 
 
 def status():
