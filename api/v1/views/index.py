0b10a2e8401f2065c1dd3c629ffb50291b6a3543
Ane-A <53179280+Ane-A@users.noreply.github.com>
2023-12-03 15:06:59 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -20,10 +20,13 @@ def status():
 
 @app_views.route("/stats", methods=["GET"], strict_slashes=False)
 def get_stats():
-    """
-    Retrieves the number of each object by type.
-    """
-    object_counts = {}
-    for key in storage.all().keys():
-        object_counts[key] = storage.count(key)
-    return jsonify(object_counts)
+ 
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
